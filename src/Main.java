19049c9b5171ab0a5f45a64f9615567e00faf705
ygtorkc <109255432+ygtorkc@users.noreply.github.com>
2023-02-11 23:09:43 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+            public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Sayı girin: ");
+                int num = input.nextInt();
+                int temp = num;
+                int rev = 0;
+                while (temp != 0) {
+                    int digit = temp % 10;
+                    rev = rev * 10 + digit;
+                    temp /= 10;
+                }
+                if (num == rev) {
+                    System.out.println(num + " palindrom bir sayıdır.");
+                } else {
+                    System.out.println(num + " palindrom bir sayı değildir.");
+                }
+                input.close();
+            }
+        }
+        ;
+    }
