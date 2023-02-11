14732ca42abff7da17394430e1f2eb40d1e7b2cf
ygtorkc <109255432+ygtorkc@users.noreply.github.com>
2023-02-11 23:11:07 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-            public static void main(String[] args) {
+           
                 Scanner input = new Scanner(System.in);
                 System.out.print("Sayı girin: ");
                 int num = input.nextInt();
@@ -22,5 +22,5 @@ public class Main {
                 input.close();
             }
         }
-        ;
-    }
+        
+    
